4d45affd06a453ec46942d835ba15c02384103bb
Purav Manot <manot.purav@gmail.com>
2022-06-14 17:43:37 +0530

    Initial Commit

---
+++ Perfect-Arrows/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Perfect-Arrows
+//
+//  Created by Purav Manot on 14/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Perfect-Arrows/Perfect_ArrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perfect_ArrowsApp.swift
+//  Perfect-Arrows
+//
+//  Created by Purav Manot on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Perfect_ArrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
