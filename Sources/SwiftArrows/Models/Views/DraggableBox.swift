1590596027aa5caf9a9a8f4638daed4b6161b845
Purav Manot <60108184+pmanot@users.noreply.github.com>
2023-02-07 18:17:49 +0530

    Update package

--- Sources/SwiftArrows/Models/Views/DraggableBox.swift
+++ Sources/SwiftArrows/Models/Views/DraggableBox.swift
@@ -41,3 +41,34 @@ public struct DraggableBox<Content: View>: View {
             )
     }
 }
+
+public struct Draggable<Content: View>: View {
+    //@Binding var location: CGPoint
+    @Binding var position: CGPoint
+    @State private var offset: CGSize = .zero
+    var content: () -> Content
+    
+    public init(position: Binding<CGPoint>, content: @escaping () -> Content) {
+        self._position = position
+        self.content = content
+    }
+    
+    public var body: some View {
+        content()
+            .position(position)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                       // withAnimation(.spring().speed(1.5)) {
+                        offset = value.translation
+                        self.position = position.offset(offset)
+                       // }
+                    }
+                    .onEnded { value in
+                        position = CGPoint(x: value.translation.width + position.x, y: value.translation.height + position.y)
+                        offset = .zero
+                        //location = position
+                    }
+            )
+    }
+}
